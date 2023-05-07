fac6cad31bfb14da42cc30d74abfa2c310917ed7
김이도 <kimido987@gmail.com>
2023-05-08 08:10:04 +0900

    [Bronze V] Title: 윤년, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.py
+++ 백준/Bronze/2753. 윤년/윤년.py
@@ -1,5 +1,2 @@
 a = int(input())
-if (a % 4 == 0) and (a % 100 != 0) or (a % 400 == 0):
-    print('1')
-else:
-    print('0')
+print('1') if ((a%4 == 0) and (a%100 != 0)) or (a%400 == 0) else print('0')
