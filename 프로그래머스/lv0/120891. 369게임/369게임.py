e210a1ea5c5e68c673c8152ba959787b2b7beab7
김이도 <kimido987@gmail.com>
2023-06-10 08:20:36 +0900

    [level 0] Title: 369게임, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.py
+++ 프로그래머스/lv0/120891. 369게임/369게임.py
@@ -1,8 +1,2 @@
 def solution(order):
-    cnt = 0
-    
-    for i in str(order):
-        if i in ['3', '6', '9']:
-            cnt += 1
-            
-    return cnt
+    return len([i for i in str(order) if i in ['3', '6', '9']])
