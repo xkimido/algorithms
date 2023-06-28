fd31bc150d5f8960c36b335c0042a4b2e65cbd0a
김이도 <kimido987@gmail.com>
2023-06-28 23:39:54 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.py
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.py
@@ -1,6 +1,2 @@
 def solution(n):
-    for i in range(1, n + 1):
-        if i * i == n:
-            return 1
-        elif i == n:
-            return 2
+    return 1 if (n ** 0.5).is_integer() else 2
