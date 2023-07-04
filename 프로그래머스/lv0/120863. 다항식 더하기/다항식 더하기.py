8d4c089264ad5c39a30c23a4c17cc721851df252
김이도 <kimido987@gmail.com>
2023-07-04 13:46:09 +0900

    [level 0] Title: 다항식 더하기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.py
@@ -0,0 +1,16 @@
+def solution(polynomial):
+    xnum = 0
+    const = 0
+    for c in polynomial.split(' + '):
+        if c.isdigit():
+            const+=int(c)
+        else:
+            xnum = xnum+1 if c=='x' else xnum+int(c[:-1])
+    if xnum == 0:
+        return str(const)
+    elif xnum==1:
+        return 'x + '+str(const) if const!=0 else 'x'
+    else:
+        return f'{xnum}x + {const}' if const!=0 else f'{xnum}x'
+    
+    # https://velog.io/@greene/프로그래머스-LEVEL0-다항식-더하기-파이썬
