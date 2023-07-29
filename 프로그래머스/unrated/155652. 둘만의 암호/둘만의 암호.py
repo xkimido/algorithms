11aad78df99db27fc8c4e0a7ef7d1b33fc6b18b3
김이도 <kimido987@gmail.com>
2023-07-30 00:08:40 +0900

    [unrated] Title: 둘만의 암호, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.py
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.py
@@ -1,9 +1,16 @@
+from string import ascii_lowercase
+
 def solution(s, skip, index):
-    alpha = "abcdefghijklmnopqrstuvwxyz"
-    answer = ""
-    for i in list(skip):
-        alpha = alpha.replace(i,"")
-        
-    for a in s:
-        answer += alpha[(alpha.find(a) + index) % len(alpha)]
-    return answer
+    result = ''
+
+    a_to_z = set(ascii_lowercase)
+    a_to_z -= set(skip)
+    a_to_z = sorted(a_to_z)
+    l = len(a_to_z)
+
+    dic_alpha = {alpha:idx for idx, alpha in enumerate(a_to_z)}
+
+    for i in s:
+        result += a_to_z[(dic_alpha[i] + index) % l]
+
+    return result
