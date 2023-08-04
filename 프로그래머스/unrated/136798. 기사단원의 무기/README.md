82d8a0ba5a451c57c0b512be82ee03ed2a56d5d0
김이도 <kimido987@gmail.com>
2023-08-04 18:00:23 +0900

    [unrated] Title: 기사단원의 무기, Time: 4.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/136798. 기사단원의 무기/README.md
+++ 프로그래머스/unrated/136798. 기사단원의 무기/README.md
@@ -1,10 +1,10 @@
 # [unrated] 기사단원의 무기 - 136798 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/136798) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/136798?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 1.84 ms
+메모리: 10.1 MB, 시간: 4.03 ms
 
 ### 구분
 
--- 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.py
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.py
@@ -1,15 +1,10 @@
-def solution(num, lim, pow):
-    div = [1]
-    for i in range(2, num+1):
-        cnt = 0
-        for j in range(1, int(i**(1/2)+1)):
-            if i % j == 0:
-                cnt += 1
-                if j**2 != i:
-                    cnt+=1
-        if cnt > lim:
-            cnt = pow
-            div.append(cnt)
-        else:    
-            div.append(cnt)
-    return sum(div)
+def cf(n): # 공약수 출력
+    a = []
+    for i in range(1,int(n**0.5)+1):
+        if n%i == 0:
+            a.append(n//i)
+            a.append(i)
+    return len(set(a))
+
+def solution(number, limit, power):
+    return sum([cf(i) if cf(i)<=limit else power for i in range(1,number+1)])
