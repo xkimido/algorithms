d21a2ac1f11604d9428a82aa7b95938b4337ace4
김이도 <kimido987@gmail.com>
2023-08-15 13:46:22 +0900

    [unrated] Title: 추억 점수, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.py
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.py
@@ -1,12 +1,2 @@
 def solution(name, yearning, photo):
-    result = []
-    name_to_yearning = {n: y for n, y in zip(name, yearning)}
-
-    for p in photo:
-        total_yearning = 0
-        for person in p:
-            if person in name_to_yearning:
-                total_yearning += name_to_yearning[person]
-        result.append(total_yearning)
-
-    return result
+    return [sum(yearning[name.index(j)] for j in i if j in name) for i in photo]
