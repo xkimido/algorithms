abf359cf6d9b659914d99b251880d0814109e077
김이도 <kimido987@gmail.com>
2023-08-23 22:01:11 +0900

    [level 1] Title: 삼총사, Time: 0.03 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.py
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.py
@@ -1,3 +1,3 @@
 from itertools import combinations
 def solution(number):
-    return sum([1 for c in list(combinations(number,3)) if not sum(c)])
+    return len([case for case in combinations(number, 3) if sum(case) == 0])
