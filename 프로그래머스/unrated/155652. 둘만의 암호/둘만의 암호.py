69a4ee27abce2861c5962b678e43c114a3745256
김이도 <kimido987@gmail.com>
2023-08-26 00:02:54 +0900

    [unrated] Title: 둘만의 암호, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.py
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.py
@@ -1,16 +1,3 @@
-from string import ascii_lowercase
-
 def solution(s, skip, index):
-    result = ''
-
-    a_to_z = set(ascii_lowercase)
-    a_to_z -= set(skip)
-    a_to_z = sorted(a_to_z)
-    l = len(a_to_z)
-
-    dic_alpha = {alpha:idx for idx, alpha in enumerate(a_to_z)}
-
-    for i in s:
-        result += a_to_z[(dic_alpha[i] + index) % l]
-
-    return result
+    alphas = [chr(a) for a in range(ord("a"), ord("z")+1) if chr(a) not in skip]
+    return "".join([alphas[(alphas.index(a) + index) % len(alphas)] for a in s])
