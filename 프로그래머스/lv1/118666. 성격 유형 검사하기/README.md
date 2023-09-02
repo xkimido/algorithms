33ad0337795d5829dc3e16878bb020bbf2f51f91
김이도 <kimido987@gmail.com>
2023-09-03 00:23:07 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.13 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/README.md
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 성격 유형 검사하기 - 118666 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118666) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118666?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.13 ms
+메모리: 10.3 MB, 시간: 0.13 ms
 
 ### 구분
 
--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -1,15 +1,17 @@
+from collections import defaultdict
+
 def solution(survey, choices):
+    indicator = [('R', 'T'), ('C', 'F'), ('J', 'M'), ('A', 'N')]
     answer = ''
-    dic= {"R" : 0,"T" : 0,"C" : 0,"F" : 0,"J" : 0,"M" : 0,"A" : 0,"N" : 0 }
-    
-    for s,c in zip(survey,choices):
-        if c>4:     dic[s[1]] += c-4
-        elif c<4:   dic[s[0]] += 4-c
-    
-    li = list(dic.items())
-    
-    for i in range(0,8,2):
-        if li[i][1] < li[i+1][1]: answer += li[i+1][0]
-        else:   answer += li[i][0]
-        
+    personality = defaultdict(int)
+    for s, c in zip(survey, choices):
+        if c < 4:
+            personality[s[0]] += (4 - c)
+        elif c > 4:
+            personality[s[1]] += (c - 4)
+    for i in indicator:
+        if personality[i[0]] >= personality[i[1]]:
+            answer += i[0]
+        else:
+            answer += i[1]
     return answer
