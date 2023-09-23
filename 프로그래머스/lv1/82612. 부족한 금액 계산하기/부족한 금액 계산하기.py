818e76d112b80226ff7cc055c5f52a374cb3d97a
Kim Ido <kimido987@gmail.com>
2023-09-24 02:53:33 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.py
+++ 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.py
@@ -1,9 +1,2 @@
 def solution(price, money, count):
-    # answer = -1
-    pay = 0
-    for i in range(1, count+1):
-        pay += (price * i)
-    if money < pay:
-        return pay - money
-    else:
-        return 0
+    return max(price*count*(count+1)/2-money,0)
