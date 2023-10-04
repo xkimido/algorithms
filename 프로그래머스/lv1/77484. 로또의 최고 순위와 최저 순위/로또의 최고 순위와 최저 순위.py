addc3eb89cf56ddc1eddc8e3b5756c0746d0257d
Kim Ido <kimido987@gmail.com>
2023-10-05 00:18:16 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.00 ms, Memory: 9.97 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
@@ -1,8 +1,8 @@
 def solution(lottos, win_nums):
 
-    rank=[6,6,5,4,3,2,1] # 딕셔너리 쓰지않고 순서대로 당첨 내용 리스트로!
+    rank=[6,6,5,4,3,2,1]
 
-    cnt_0 = lottos.count(0) # 찾고자 하는 항목이 파이썬의 리스트에 몇개나 들어있는지 확인하는 count 함수
+    cnt_0 = lottos.count(0)
     ans = 0
     for x in win_nums:
         if x in lottos:
