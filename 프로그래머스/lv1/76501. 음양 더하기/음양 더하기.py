3ac9b3033d1b283a6fca747709f53541dc495fba
Kim Ido <kimido987@gmail.com>
2023-10-08 03:50:08 +0900

    [level 1] Title: 음양 더하기, Time: 0.13 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.py
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.py
@@ -1,8 +1,2 @@
 def solution(absolutes, signs):
-    answer = 0
-    for i in range(len(absolutes)): 
-        if signs[i]:
-            answer += absolutes[i]
-        else:
-            answer -= absolutes[i]
-    return answer
+    return sum(absolutes if sign else -absolutes for absolutes, sign in zip(absolutes, signs))
