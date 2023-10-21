d2868322f5f39f9be0f3ecf7710a3453f9a8798e
Kim Ido <kimido987@gmail.com>
2023-10-22 00:10:46 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.35 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
@@ -1,33 +1,40 @@
 def solution(numbers, hand):
     answer = ''
-    lpos = [1,4] # means *
-    rpos = [3,4] # means #
-    for i in numbers:
-        if i==1 or i==4 or i==7:
-            answer+='L'
-            lpos = [1,(i-1)/3+1]
-        elif i==3 or i==6 or i==9:
-            answer+='R'
-            rpos = [3,i/3]
+    key_dict = {1:(0,0),2:(0,1),3:(0,2),
+                4:(1,0),5:(1,1),6:(1,2),
+                7:(2,0),8:(2,1),9:(2,2),
+                '*':(3,0),0:(3,1),'#':(3,2)}
 
+    left = [1,4,7]
+    right = [3,6,9]
+    lhand = '*'
+    rhand = '#'
+    for i in numbers:
+        if i in left:
+            answer += 'L'
+            lhand = i
+        elif i in right:
+            answer += 'R'
+            rhand = i
         else:
-            if i==2: temp = [2,1]
-            if i==5: temp = [2,2]
-            if i==8: temp = [2,3]
-            if i==0: temp = [2,4]
-            ldis = abs(temp[0]-lpos[0])+abs(temp[1]-lpos[1])
-            rdis = abs(temp[0]-rpos[0])+abs(temp[1]-rpos[1])
-            if ldis<rdis:
-                answer+='L'
-                lpos = temp
-            elif ldis>rdis:
-                answer+='R'
-                rpos =temp
+            curPos = key_dict[i]
+            lPos = key_dict[lhand]
+            rPos = key_dict[rhand]
+            ldist = abs(curPos[0]-lPos[0]) + abs(curPos[1]-lPos[1])
+            rdist = abs(curPos[0]-rPos[0]) + abs(curPos[1]-rPos[1])
+
+            if ldist < rdist:
+                answer += 'L'
+                lhand = i
+            elif ldist > rdist:
+                answer += 'R'
+                rhand = i
             else:
-                if hand == "right":
-                    answer+='R'
-                    rpos = temp
+                if hand == 'left':
+                    answer += 'L'
+                    lhand = i
                 else:
-                    answer+='L'
-                    lpos = temp
+                    answer += 'R'
+                    rhand = i
+
     return answer
