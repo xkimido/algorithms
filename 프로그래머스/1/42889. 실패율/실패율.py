081938c76fa46d2cb97cae957d4362a60f2e0572
Kim Ido <kimido987@gmail.com>
2023-10-30 15:35:44 +0900

    [level 1] Title: 실패율, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -1,16 +1,10 @@
 def solution(N, stages):
-    answer = []
-    fail = []
-    info = [0] * (N + 2)
-    for stage in stages:
-        info[stage] += 1
-    for i in range(N):
-        be = sum(info[(i + 1):])
-        yet = info[i + 1]
-        if be == 0:
-            fail.append((str(i + 1), 0))
-        else:
-            fail.append((str(i + 1), yet / be))
-    for item in sorted(fail, key=lambda x: x[1], reverse=True):
-        answer.append(int(item[0]))
+    fail = {}
+    for i in range(1,N+1):
+        try:
+            fail_ = len([a for a in stages if a==i])/len([a for a in stages if a>=i])
+        except:
+            fail_ = 0
+        fail[i]=fail_
+    answer = sorted(fail, key=fail.get, reverse=True)
     return answer
