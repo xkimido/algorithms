6b6c9d7a5b50ca79c27ba6de4ac93ffdd37338be
Kim Ido <kimido987@gmail.com>
2023-11-27 00:45:49 +0900

    [level 1] Title: 모의고사, Time: 10.17 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 2.49 ms
+메모리: 10.5 MB, 시간: 10.17 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 0일 0:38:7
+2023년 11월 1일 0:45:43
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,22 +1,44 @@
 def solution(answers):
-    
+    number_one = []
+    number_two = []
+    number_three = []
     answer = []
-    score = [0,0,0]
-    
-    student1 = [1,2,3,4,5]
-    student2 = [2,1,2,3,2,4,2,5]
-    student3 = [3,3,1,1,2,2,4,4,5,5]
-    
-    for i in range(len(answers)) :
-        if answers[i] == student1[i%5] :
-            score[0] += 1
-        if answers[i] == student2[i%8] :
-            score[1] += 1
-        if answers[i] == student3[i%10] :
-            score[2] += 1
-        
-    for idx, num in enumerate(score) :
-        if num == max(score) :
-            answer.append(idx +1)
-    
+    for i in range(len(answers)):
+        number_one.append((i+1)%5 or 5)
+        if (i+1) % 2:
+            number_two.append(2)
+        elif (i+1) % 8 == 2:
+            number_two.append(1)
+        elif (i+1) % 8 == 4:
+            number_two.append(3)
+        elif (i+1) % 8 == 6:
+            number_two.append(4)
+        elif not (i+1)%8:
+            number_two.append(5)
+        if (i+1) % 10 == 1 or (i+1) % 10 == 2:
+            number_three.append(3)
+        elif (i+1) % 10 == 3 or (i+1) % 10 == 4:
+            number_three.append(1)
+        elif (i+1) % 10 == 5 or (i+1) % 10 == 6:
+            number_three.append(2)
+        elif (i+1) % 10 == 7 or (i+1) % 10 == 8:
+            number_three.append(4)
+        elif (i+1) % 10 == 9 or (i+1) % 10 == 0:
+            number_three.append(5)
+    correct_one,correct_two,correct_three = 0,0,0
+    for i in range(len(answers)):
+        if answers[i] == number_one[i]:
+            correct_one += 1 
+        if answers[i] == number_two[i]:
+            correct_two += 1 
+        if answers[i] == number_three[i]:
+            correct_three += 1
+    correct = [correct_one, correct_two, correct_three]
+    if max(correct) == correct_one:
+        answer.append(1)
+    if max(correct) == correct_two:
+        answer.append(2)
+    if max(correct) == correct_three:
+        answer.append(3)
+    answer.sort()
     return answer
