17843e94ec6a7d6b0b6bf27056add3e13d4b9296
Kim Ido <kimido987@gmail.com>
2023-12-30 18:17:44 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 69.16 ms, Memory: 26.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=python3) 
 
 ### 성능 요약
 
-메모리: 26.3 MB, 시간: 66.93 ms
+메모리: 26.3 MB, 시간: 69.16 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 5일 23:2:11
+2023년 12월 6일 18:17:40
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,12 +1,7 @@
 def solution(participant, completion):
-    answer = ''
     participant.sort()
     completion.sort()
-    
-    for i, j in zip(participant, completion):
-        if i!=j:
-            return i
-    
-    
-    
-    return participant[-1]
+    for i in range(len(completion)):
+        if participant[i] != completion[i]:
+            return participant[i]
+    return participant[len(participant)-1]
