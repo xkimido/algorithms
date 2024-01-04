8148773919669ab831840379fde86f8217e4e36b
Kim Ido <kimido987@gmail.com>
2024-01-04 23:13:35 +0900

    [level 1] Title: 소수 찾기, Time: 308.58 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -1,6 +1,7 @@
 def solution(n):
-    a=set(range(3,n+1,2))
-    for i in range(3,n+1,2):
-        if i in a:
-            a-=set(range(i*2,n+1,i))
-    return len(a)+1
+    num=set(range(2,n+1))
+
+    for i in range(2,n+1):
+        if i in num:
+            num-=set(range(2*i,n+1,i))
+    return len(num)
