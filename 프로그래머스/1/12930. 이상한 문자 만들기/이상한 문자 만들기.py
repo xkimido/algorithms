d74c3226f296722868f891d79a58c8a01c0cdacb
Kim Ido <kimido987@gmail.com>
2024-01-11 12:35:21 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.06 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -1,11 +1,16 @@
 def solution(s):
-    answer = ''
-    for word in s.split(" "):
-        n = ''
-        for i in range(len(word)):
-            if i%2 == 0 :
-                n += word[i].upper()
-            else :
-                n += word[i].lower()
-        answer += (n + " ")
-    return answer[0:-1]
+    arr= s.split(" ")
+    answer_list = []
+    for i in range(len(arr)):
+        temp = arr[i]
+        temp_list = list(map(str,temp))
+        for j in range(len(temp_list)):
+            if j % 2 == 0:
+                temp_list[j] = temp_list[j].upper()
+            else:
+                temp_list[j] = temp_list[j].lower()
+        item = "".join(temp_list)
+        answer_list.append(item)
+
+    answer = " ".join(answer_list)
+    return answer
