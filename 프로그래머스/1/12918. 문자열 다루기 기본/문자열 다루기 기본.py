7891f1e70638f65e1eb3167149b03390d6a24dae
Kim Ido <kimido987@gmail.com>
2024-01-18 20:14:20 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
@@ -1,10 +1,2 @@
 def solution(s):
-    l = len(s)
-    try:
-        a = int(s)
-        if l == 4 or l == 6:
-            return True
-        else:
-            return False
-    except:
-        return False
+    return s.isdigit() and len(s) in [4,6]
