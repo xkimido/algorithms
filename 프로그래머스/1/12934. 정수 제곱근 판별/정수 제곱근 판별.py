b1727e0f85eddc722036f387253d25193ca2bba3
Kim Ido <kimido987@gmail.com>
2024-02-26 00:03:52 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
@@ -1,5 +1,5 @@
 def solution(n):
-    if int(n**0.5) == n**0.5:
-        return ((n**0.5)+1)**2
-    else : 
-        return -1
+    x = n ** (1/2)
+    if x % 1 == 0:
+        return (x + 1) ** 2
+    return -1
