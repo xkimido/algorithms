7100da82a26a15abe4cbee948b8bac5ae768e33c
Kim Ido <kimido987@gmail.com>
2024-03-06 01:32:49 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 이웃한 칸, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/README.md
+++ 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/README.md
@@ -1,10 +1,10 @@
 # [level 1] [PCCE 기출문제] 9번 / 이웃한 칸 - 250125 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/250125) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/250125?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.01 ms
+메모리: 10.3 MB, 시간: 0.00 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 05일 00:02:13
+2024년 03월 06일 01:32:46
 
 ### 문제 설명
 
--- 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.py
+++ 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.py
@@ -1,13 +1,13 @@
 def solution(board, h, w):
-    dy = [0,-1,0,1]
-    dx = [-1,0,1,0]
     answer = 0
     
-    for i in range(4):
-        nh = h + dy[i]
-        nw = w + dx[i]
-        if 0<= nh < len(board) and 0<= nw < len(board[0]):
-            if board[nh][nw] == board[h][w]:
-                answer += 1
-    
+    if 0 < h:
+        if board[h][w] == board[h-1][w]: answer += 1
+    if 0 < w:
+        if board[h][w] == board[h][w-1]: answer += 1
+    if h < len(board) - 1:
+        if board[h][w] == board[h+1][w]: answer += 1
+    if w < len(board[0]) - 1:
+        if board[h][w] == board[h][w+1]: answer += 1
+
     return answer
