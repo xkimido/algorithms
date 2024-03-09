df71ef6b0dc6b570a69b46d8c15b2187beb8aaae
Kim Ido <kimido987@gmail.com>
2024-03-10 00:04:23 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -1,13 +1,12 @@
 def solution(id_list, report, k):
-    answer = [0] * len(id_list)
-    dic_report = {id: [] for id in id_list}
-    for i in set(report):
-        i = i.split()
-        dic_report[i[1]].append(i[0])
+    answer = [0] * len(id_list)    
+    reports = {x : 0 for x in id_list}
 
-    for key, value in dic_report.items():
-        if len(value) >= k:
-            for j in value:
-                answer[id_list.index(j)] += 1
+    for r in set(report):
+        reports[r.split()[1]] += 1
+
+    for r in set(report):
+        if reports[r.split()[1]] >= k:
+            answer[id_list.index(r.split()[0])] += 1
 
     return answer
