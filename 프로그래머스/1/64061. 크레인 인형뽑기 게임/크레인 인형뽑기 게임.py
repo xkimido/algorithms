8729825af22b1be9588bfda0b3ffefd5f2a752de
Kim Ido <kimido987@gmail.com>
2024-03-16 00:43:58 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.26 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
@@ -1,18 +1,12 @@
 def solution(board, moves):
-    stacklist = []
-    answer = 0
+    cols = list(map(lambda x: list(filter(lambda y: y > 0, x)), zip(*board)))
+    a, s = 0, [0]
 
-    for i in moves:
-        for j in range(len(board)):
-            if board[j][i-1] != 0:
-                stacklist.append(board[j][i-1])
-                board[j][i-1] = 0
+    for m in moves:
+        if len(cols[m - 1]) > 0:
+            if (d := cols[m - 1].pop(0)) == (l := s.pop()):
+                a += 2
+            else:
+                s.extend([l, d])
 
-                if len(stacklist) > 1:
-                    if stacklist[-1] == stacklist[-2]:
-                        stacklist.pop(-1)
-                        stacklist.pop(-1)
-                        answer += 2     
-                break
-
-    return answer
+    return a
