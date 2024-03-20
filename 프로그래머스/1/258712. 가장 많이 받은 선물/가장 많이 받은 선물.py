63167209d503fe286780e9858833a6d552a708da
Kim Ido <kimido987@gmail.com>
2024-03-21 00:20:30 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 5.23 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
@@ -1,20 +1,19 @@
 def solution(friends, gifts):
-    f = {v: i for i, v in enumerate(friends)}
-    l = len(friends)
-    p = [0] * l
-    answer = [0] * l
-    gr = [[0] * l for i in range(l)]
-    for i in gifts:
-        a, b = i.split()
-        gr[f[a]][f[b]] += 1
-    for i in range(l):
-        p[i] = sum(gr[i]) - sum([k[i] for k in gr])
-
-    for i in range(l):
-        for j in range(l):
-            if gr[i][j] > gr[j][i]:
-                answer[i] += 1
-            elif gr[i][j] == gr[j][i]:
-                if p[i] > p[j]:
-                    answer[i] += 1
-    return max(answer)
+    d={}
+    for i in friends:
+        d[i]=dict(zip(friends,[0]*len(friends))) # -:받기 +:주기
+    for j in gifts:
+        a,b=j.split()
+        d[a][b]+=1
+        d[b][a]-=1
+    d2=dict(zip(friends,[0]*len(friends)))
+    for p in friends:
+        for q in friends:
+            if p==q:
+                continue
+            if d[p][q]>0:
+                d2[p]+=1
+            elif d[p][q]==0:
+                if sum(list(d[p].values()))>sum(list(d[q].values())):
+                    d2[p]+=1
+    return max(list(d2.values()))
