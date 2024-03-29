4c291cf123cc21963cad15cbcb14cacfeba69f0b
Kim Ido <kimido987@gmail.com>
2024-03-29 10:33:13 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.21 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
@@ -1,12 +1,31 @@
+from collections import deque
+
+
 def solution(bandage, health, attacks):
+    end = attacks[-1][0]
+    attacks = deque(attacks)
     hp = health
-    start = 1
-    for i, j in attacks:
-        hp += ((i - start) // bandage[0]) * bandage[2] + (i - start) * bandage[1]
-        start = i + 1
-        if hp >= health:
-            hp = health
-        hp -= j
-        if hp <= 0:
+
+    time = 0
+    for t in range(1, end + 1):
+        if health <= 0:
             return -1
-    return hp
+
+        if not attacks:
+            break
+        time += 1
+        if t == attacks[0][0]:
+            health -= attacks.popleft()[1]
+            time = 0
+
+        elif time == bandage[0]:
+            health += bandage[1] + bandage[2]
+            time = 0
+
+        else:
+            health += bandage[1]
+
+        if health > hp:
+            health = hp
+
+    return health if health > 0 else -1
