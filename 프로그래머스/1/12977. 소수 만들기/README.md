eb4c4d0b2070a793e12d7e5758982ab521ab445d
Kim Ido <kimido987@gmail.com>
2024-04-12 00:09:31 +0900

    [level 1] Title: 소수 만들기, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/README.md
+++ 프로그래머스/1/12977. 소수 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 소수 만들기 - 12977 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12977) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12977?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.02 ms
+메모리: 10 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 11일 21:41:50
+2024년 04월 12일 00:09:28
 
 ### 문제 설명
 
--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
@@ -1,14 +1,11 @@
-from itertools import combinations
-
 def solution(nums):
+    from itertools import combinations as cb
     answer = 0
-    for i in combinations(nums, 3):
-        s = sum(i)
-        chk = True
-        for j in range(2, int(s ** 0.5) + 1):
-            if s % j == 0:
-                chk = False
+    for a in cb(nums, 3):
+        cand = sum(a)
+        for j in range(2, cand):
+            if cand%j==0:
                 break
-        if chk is True:
+        else:
             answer += 1
     return answer
