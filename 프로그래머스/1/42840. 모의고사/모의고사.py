b373efff44d23c1f9de9a60c6a2dd9babe774be7
Kim Ido <kimido987@gmail.com>
2024-04-14 00:21:30 +0900

    [level 1] Title: 모의고사, Time: 3.30 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,16 +1,20 @@
-from itertools import cycle
-
 def solution(answers):
-    giveups = [
-        cycle([1,2,3,4,5]),
-        cycle([2,1,2,3,2,4,2,5]),
-        cycle([3,3,1,1,2,2,4,4,5,5]),
-    ]
-    scores = [0, 0, 0]
-    for num in answers:
-        for i in range(3):
-            if next(giveups[i]) == num:
-                scores[i] += 1
-    highest = max(scores)
+    pattern1 = [1,2,3,4,5]
+    pattern2 = [2,1,2,3,2,4,2,5]
+    pattern3 = [3,3,1,1,2,2,4,4,5,5]
+    score = [0, 0, 0]
+    result = []
 
-    return [i + 1 for i, v in enumerate(scores) if v == highest]
+    for idx, answer in enumerate(answers):
+        if answer == pattern1[idx%len(pattern1)]:
+            score[0] += 1
+        if answer == pattern2[idx%len(pattern2)]:
+            score[1] += 1
+        if answer == pattern3[idx%len(pattern3)]:
+            score[2] += 1
+
+    for idx, s in enumerate(score):
+        if s == max(score):
+            result.append(idx+1)
+
+    return result
