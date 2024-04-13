00fbc3566d6bd85b7f834872f9aca469f8e44f65
Kim Ido <kimido987@gmail.com>
2024-04-13 23:32:58 +0900

    [level 1] Title: 폰켓몬, Time: 0.45 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,7 +1,2 @@
-def solution(nums):
-    unique_types = len(set(nums))
-
-    if len(nums) / 2 > unique_types:
-        return unique_types
-    else:
-        return len(nums) / 2
+def solution(ls):
+    return min(len(ls)/2, len(set(ls)))
