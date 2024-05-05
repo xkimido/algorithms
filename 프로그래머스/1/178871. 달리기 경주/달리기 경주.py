e9293d7e6b2cbff5b92a83c383a5ccf8d869d261
Kim Ido <kimidokr@gmail.com>
2024-05-06 01:18:32 +0900

    [level 1] Title: 달리기 경주, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
@@ -1,12 +1,19 @@
 def solution(players, callings):
-    player_indices = {player: index for index, player in enumerate(players)}
+    answer = []
 
-    for j in callings:
-        current_index = player_indices[j]
-        desired_index = current_index - 1
-        if current_index > 0 and players[desired_index] != j:
-            players[current_index], players[desired_index] = players[desired_index], players[current_index]
-            player_indices[players[current_index]] = current_index
-            player_indices[players[desired_index]] = desired_index
+    p_dic = {player:i+1 for i,player in enumerate(players)}
+    location_dic = {i+1:player for i,player in enumerate(players)}
 
-    return players
+    for c in callings:
+        c_loc = p_dic[c]
+        front = c_loc - 1
+        front_p = location_dic[front]
+        p_dic[c] -= 1
+        p_dic[front_p] += 1
+        location_dic[c_loc] = front_p
+        location_dic[front] = c
+
+    p_dic = dict(sorted(p_dic.items(),key=lambda x:x[1]))
+    answer = list(p_dic.keys())
+
+    return answer
