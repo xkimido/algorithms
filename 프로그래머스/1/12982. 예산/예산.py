83f66208e72e02313c63ca89f96cb4c564abb633
Kim Ido <kimidokr@gmail.com>
2024-05-08 00:48:44 +0900

    [level 1] Title: 예산, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.py
+++ 프로그래머스/1/12982. 예산/예산.py
@@ -1,5 +1,13 @@
 def solution(d, budget):
+    s = 0
+    j = 0
     d.sort()
-    while budget < sum(d):
-        d.pop()
-    return len(d)
+
+    for i in range(len(d)):
+        s += d[i]
+        if s > budget:
+            break
+        else:
+            j += 1
+
+    return j
