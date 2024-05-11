51400304b6a92994be39035e910ae6969daa65d8
Kim Ido <kimidokr@gmail.com>
2024-05-12 00:07:11 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -1,8 +1,5 @@
 def solution(food):
-    answer ="0"
-    for i in range(len(food)-1, 0,-1):
-        c = int(food[i]/2)
-        while c>0:
-            answer = str(i) + answer + str(i)
-            c -= 1
-    return answer
+    answer = ''
+    for i,n in enumerate(food[1:]):
+        answer += str(i+1) * (n//2)
+    return answer + "0" + answer[::-1]
