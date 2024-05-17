a946626702a4a50c508350d676a8be4d4bb599f2
Kim Ido <kimidokr@gmail.com>
2024-05-18 00:57:55 +0900

    [level 1] Title: 둘만의 암호, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.py
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.py
@@ -1,9 +1,16 @@
-import re
+from string import ascii_lowercase
+
 def solution(s, skip, index):
-    tmp = "abcdefghijklmnopqrstuvwxyz"
-    ans = ""
-    for i in list(skip):
-        tmp = tmp.replace(i,"")
+    result = ''
+
+    a_to_z = set(ascii_lowercase)
+    a_to_z -= set(skip)
+    a_to_z = sorted(a_to_z)
+    l = len(a_to_z)
+
+    dic_alpha = {alpha:idx for idx, alpha in enumerate(a_to_z)}
+
     for i in s:
-        ans += tmp[(tmp.find(i) + index) % len(tmp)]
-    return ans
+        result += a_to_z[(dic_alpha[i] + index) % l]
+
+    return result
