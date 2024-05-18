89824213e964e0a7a91881fd51889ef2506ff108
Kim Ido <kimidokr@gmail.com>
2024-05-19 00:17:56 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.16 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
@@ -1,13 +1,8 @@
-def solution(wallpaper):
-    width, height = len(wallpaper[0]), len(wallpaper)
-    lux, luy, rdx, rdy = height, width, 0, 0
-
-    for row_idx, row in enumerate(wallpaper):
-        if "#" in row:
-            lux = min(lux, row_idx)
-            rdx = max(rdx, row_idx)
-            luy = min(luy, row.index("#"))
-            rdy = max(rdy, width - row[::-1].index("#") - 1)
-
-    answer = [lux, luy, rdx + 1, rdy + 1]
-    return answer
+def solution(wall):
+    a, b = [], []
+    for i in range(len(wall)):
+        for j in range(len(wall[i])):
+            if wall[i][j] == "#":
+                a.append(i)
+                b.append(j)
+    return [min(a), min(b), max(a) + 1, max(b) + 1]
