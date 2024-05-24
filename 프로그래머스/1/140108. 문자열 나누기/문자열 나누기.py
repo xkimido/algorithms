e6f49cf0575fa7d3b0c8c4c3cc8bdeae3f6aee90
Kim Ido <kimidokr@gmail.com>
2024-05-25 02:20:59 +0900

    [level 1] Title: 문자열 나누기, Time: 0.82 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
@@ -0,0 +1,16 @@
+def solution(s):
+    
+    answer = 0
+    cnt1 = 0
+    cnt2 = 0
+    
+    for i in s:
+        if cnt1 == cnt2:
+            answer += 1
+            x = i
+        if i == x:
+            cnt1 += 1
+        else:
+            cnt2 += 1     
+            
+    return answer
