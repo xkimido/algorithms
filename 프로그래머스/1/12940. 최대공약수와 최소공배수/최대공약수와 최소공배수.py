5d0bef3405beda5d8a7b1214605a12b77782976d
Kim Ido <kimidokr@gmail.com>
2024-06-22 00:06:01 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,14 +1,4 @@
 def solution(n, m):
-    answer = []
-    arr1 = []
-    for i in range(1, min(n, m)+1):
-        if n%i == 0 and m%i ==0:
-            arr1.append(i)
-    for i in range(max(n, m), (n*m)+1):
-        if i%n == 0 and i%m == 0:
-            min_num = i
-            break
-    max_num = max(arr1)
-    answer.append(max_num)
-    answer.append(min_num)
-    return answer
+    gcd = lambda a,b : b if not a%b else gcd(b, a%b)
+    lcm = lambda a,b : a*b//gcd(a,b)
+    return [gcd(n, m), lcm(n, m)]
