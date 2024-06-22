9df55a32329718ecafe2e2b86996ad6a060649df
Kim Ido <kimidokr@gmail.com>
2024-06-23 00:11:44 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,4 +1,8 @@
-def solution(n, m):
-    gcd = lambda a,b : b if not a%b else gcd(b, a%b)
-    lcm = lambda a,b : a*b//gcd(a,b)
-    return [gcd(n, m), lcm(n, m)]
+def solution(a, b):
+    c,d = max(a, b), min(a, b)
+    t = 1
+    while t>0:
+        t = c%d
+        c, d = d, t
+    answer = [ c, int (a*b/c)]
+    return answer
