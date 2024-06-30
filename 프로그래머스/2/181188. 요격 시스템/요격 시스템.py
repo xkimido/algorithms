d13b69d4e066290736250d478c37f2f3e789497a
Kim Ido <kimidokr@gmail.com>
2024-07-01 00:46:49 +0900

    [level 2] Title: 요격 시스템, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.py
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.py
@@ -1,11 +1,12 @@
 def solution(targets):
     answer = 0
-    targets.sort(key = lambda x: [x[1], x[0]])
-    
-    s = e = 0
-    for target in targets:
-        if target[0] >= e:
-            answer += 1
-            e = target[1]
+    targets.sort(key=lambda x: x[1])
+    temp = 0
+    for i in targets:
+        if i[0]<temp:
+            continue
+        else:
+            answer+=1
+            temp = i[1]
 
-    return answer 
+    return answer
